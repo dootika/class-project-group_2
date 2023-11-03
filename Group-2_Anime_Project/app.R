3513683e01d1cadf3a0635cdeb81fcdc32fddeee
Zehaan <zehaan22@iitk.ac.in>
2023-11-03 11:14:21 +0530

    Updated the app

--- Group-2_Anime_Project/app.R
+++ Group-2_Anime_Project/app.R
@@ -1,11 +1,8 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Reading the Data and sorting out various plot types
+MainData <- read.csv("../Data\ Scraping\ and\ Cleaning/Anime\ Dataframe.csv")
+
+# Categories of the anime and their frequency
+categories <- table(MainData$Source) 
 
 library(shiny)
 
@@ -18,18 +15,33 @@ ui <- fluidPage(
   sidebarLayout(
     # Designs the sidebar for the page (Mainly for Inputs)
     sidebarPanel(
-      h4("sidebar panel")
+      h4("Sidebar Panel"),
+      
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
       ),
+    
     
     # Designs the main panel of the main page (Mainly for plot outputs)
     mainPanel(
-      h4("main panel")
-      )
+      # Title of the Main Panel
+      h4("main panel"),
+      
+      # Plot Output - 1
+      plotOutput("distPlot")
+    )
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    # draw the histogram with the specified number of bins
+    plot(categories)
+  })
   
 }
 
