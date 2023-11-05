6db61eb1a30591ac7cc2935f6c740f8d3ab58ad9
Zehaan <zehaan22@iitk.ac.in>
2023-11-05 12:02:13 +0530

    Added the dynaic table to the app

--- Group-2_Anime_Project/app.R
+++ Group-2_Anime_Project/app.R
@@ -41,19 +41,25 @@ ui <- fluidPage(
         "SubDivParam",
         "Sub Division Parameter",
         c(
-          "Source",
           "Season",
           "Broadcast Day",
           "Genre"
         )
       ),
-
-      sliderInput("EpiCount",
-                  "Maximum Number of Episodes:",
-                  min = 1,
-                  max = 100,
-                  value = 50)
+      
+      selectInput(
+        "XaxisMetric",
+        "Indedendent Parameter",
+        c("No. Of Episodes",
+          "Studio",
+          "Duration per Episode",
+          "Broadcast Time"
+        )
       ),
+      # Reactive Slider Input
+      uiOutput("CountInput"),
+      ),
+    
     
     
     # Designs the main panel of the main page (Mainly for plot outputs)
@@ -62,9 +68,9 @@ ui <- fluidPage(
       h4("Main Panel"),
     
       tabsetPanel(
-        tabPanel("Plot", plotOutput("distPlot")),
-        tabPanel("Summary", verbatimTextOutput("summary")),
-        tabPanel("Table", tableOutput("table"))
+        tabPanel("Scatter Plot", plotOutput("ScatPlot")),
+        tabPanel("Bar Plot", plotOutput("Barplot")),
+        tabPanel("Table", tableOutput("TablePlot"))
       )
       
     )
@@ -75,14 +81,31 @@ ui <- fluidPage(
 
 # Define server logic ----
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    subdata <- subset(MainData, (MainData$Episode.Count<input$EpiCount)&(MainData$Rating == input$AgeGroup) )
+  output$ScatPlot <- renderPlot({
+    subdata <- subset(MainData, 
+                      switch (
+                        input$XaxisMetric,
+                        `No. Of Episodes` = (MainData$Episode.Count<input$Count),
+                        `Studio` = (MainData$Studio == input$Count),
+                        `Duration per Episode` = (MainData$Duration.per.Episode<input$Count),
+                        `Broadcast Time` = TRUE
+                      )&
+                      (MainData$Rating == input$AgeGroup)&
+                      (MainData$Source == input$Source)
+                      )
     Y_Param <- switch (input$YaxisMetric,
       Rating = subdata$Score,
       `No. of Votes` = subdata$Scored.by,
       Viewers = subdata$Members,
       Favourites = subdata$Favorites
       
+    )
+    X_Param <- switch (input$XaxisMetric,
+                       `No. Of Episodes` = subdata$Episode.Count,
+                       `Studio` = subdata$Studio,
+                       `Duration per Episode` = subdata$Duration.per.Episode,
+                       `Broadcast Time` = subdata$Broadcast.JST
+                       
     )
     Subdiv_Param <- switch (input$SubDivParam,
                             `Source` = subdata$Source,
@@ -92,10 +115,63 @@ server <- function(input, output) {
                        
     )
     # draw the plot
-    ggplot(subdata, mapping = aes(Episode.Count, Y_Param))+
-           geom_point(mapping = aes( col = Subdiv_Param))
+    ggplot(subdata, 
+           mapping = aes(X_Param, Y_Param),
+           )+
+      geom_point(mapping = aes( col = Subdiv_Param))+
+      labs(y= input$YaxisMetric, x = input$XaxisMetric, col = input$SubDivParam)
   })
   
+  output$CountInput <- renderUI(
+    switch (
+      input$XaxisMetric,
+      `No. Of Episodes` = sliderInput("Count",
+                                      "Maximum Number of Episodes:",
+                                      min = 1,
+                                      max = 100,
+                                      value = 50),
+      `Studio` = checkboxGroupInput("Count",
+                                    "Select Studios:",
+                                    unique(MainData$Studio)),
+      `Duration per Episode` = sliderInput("Count",
+                                           "Max Duration Per Episode:",
+                                           min = 1,
+                                           max = 100,
+                                           value = 50),
+      `Broadcast Time` = ''
+                                     
+  ))
+  
+  output$Barplot <- renderPlot({
+    # draw the plot
+    ggplot(MainData, 
+           mapping = aes(MainData$Score),
+    )+
+      geom_histogram(mapping = aes(col = MainData$Genre))+
+      labs(y= "No. of Observations", x = "Rating", col = "Season")
+  })
+  
+  output$TablePlot <- renderTable({
+    subdata <- subset(MainData, 
+                      switch (
+                        input$XaxisMetric,
+                        `No. Of Episodes` = (MainData$Episode.Count<input$Count),
+                        `Studio` = (MainData$Studio == input$Count),
+                        `Duration per Episode` = (MainData$Duration.per.Episode<input$Count),
+                        `Broadcast Time` = TRUE
+                      )&
+                        (MainData$Rating == input$AgeGroup)&
+                        (MainData$Source == input$Source)
+    )
+    newDF <- data.frame(
+      Rank = subdata$Rank,
+      Title = subdata$Anime.Title,
+      Rating = subdata$Score,
+      `Active Viewers` = subdata$Scored.by
+    )
+    newDF
+  })
+
 }
 
 # Run the app ----
